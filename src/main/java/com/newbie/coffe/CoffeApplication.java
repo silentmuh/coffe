2763ede5d1129e1c1a23b181b2e9167dd826f5b8
ZYF1012 <github.com.1012>
2019-05-05 16:43:26 +0800

    test

--- src/main/java/com/newbie/coffe/CoffeApplication.java
+++ src/main/java/com/newbie/coffe/CoffeApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoffeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CoffeApplication.class, args);
+
     }
 
 }
